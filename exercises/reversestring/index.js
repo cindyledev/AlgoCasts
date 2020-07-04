2713f384d83b14f6f704f945103fd7574aff0cac
Cindy Le <cindyledev@gmail.com>
2020-07-04 09:28:44 -0400

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //   return str.split("").reduce((rev, char) => {
+  //     return char + rev;
+  //   }, "");
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
+
+// Solution 1
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+//   return str.split("").reverse().join("");
+// }
+
+// Solution 2
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Solution 3
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => {
+//     return char + rev;
+//   }, "");
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
