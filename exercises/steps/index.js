b078f2d921dc2eb5690be0d0deb260dc3541b046
Cindy Le <cindyledev@gmail.com>
2020-07-06 14:26:28 -0400

    Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,58 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
+
+// Solution 1
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+// Solution 2
+// function steps(n, row = 0, stair = "") {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+// //   if (stair.length <= row) {
+// //     stair += "#";
+// //   } else {
+// //     stair += " ";
+// //   }
+
+//   const add = stair.length <= row ? "#" : " ";
+//   steps(n, row, stair + add);
+// }
